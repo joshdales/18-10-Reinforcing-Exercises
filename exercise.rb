54287e916f445b81966eb7d975a223d649420158
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 10:05:33 -0400

    Add to method to make numbers ending in 2 end in nd

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,13 @@
 def ordinal_indicator(num)
    str = num.to_s
-  if str[-1].include? "1"
+  if str[-1] == "1"
     oi = str + "st"
+  elsif str[-1] == "2"
+    oi = str + "nd"
   end
   oi
 end
 
 
 puts ordinal_indicator(1)
+puts ordinal_indicator(2)
