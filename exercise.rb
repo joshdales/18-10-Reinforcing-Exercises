1bf0e511de7bf30662bda7ed5426ec6ef2110537
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 10:18:47 -0400

    Add rule that makes 11, 12, 13 end in th

--- exercise.rb
+++ exercise.rb
@@ -1,6 +1,8 @@
 def ordinal_indicator(num)
    str = num.to_s
-  if str[-1] == "1"
+  if str[-2..-1] == "11" || str[-2..-1] ==  "12" || str[-2..-1] ==  "13"
+   oi = str + "th"
+ elsif str[-1] == "1"
     oi = str + "st"
   elsif str[-1] == "2"
     oi = str + "nd"
@@ -19,3 +21,9 @@ puts ordinal_indicator(3)
 puts ordinal_indicator(4)
 puts ordinal_indicator(5)
 puts ordinal_indicator(10)
+puts ordinal_indicator(11)
+puts ordinal_indicator(12)
+puts ordinal_indicator(13)
+puts ordinal_indicator(21)
+puts ordinal_indicator(22)
+puts ordinal_indicator(23)
