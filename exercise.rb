1ce90b555318fc789df36b30f7935a880fdb8bed
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 10:09:19 -0400

    Add to method to make numbers ending in 3 end in rd and others end in th

--- exercise.rb
+++ exercise.rb
@@ -4,6 +4,10 @@ def ordinal_indicator(num)
     oi = str + "st"
   elsif str[-1] == "2"
     oi = str + "nd"
+  elsif str[-1] == "3"
+    oi = str + "rd"
+  else
+    oi = str + "th"
   end
   oi
 end
@@ -11,3 +15,7 @@ end
 
 puts ordinal_indicator(1)
 puts ordinal_indicator(2)
+puts ordinal_indicator(3)
+puts ordinal_indicator(4)
+puts ordinal_indicator(5)
+puts ordinal_indicator(10)
